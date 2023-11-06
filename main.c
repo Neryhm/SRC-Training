e55313c8a17871830bdc64297e0960ce165df216
SnappyS05 <thinhnguyentien05@gmail.com>
2023-11-06 23:17:51 +0700

    SRC_TRAINING

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
+
+/**
+ * @file main.cpp
+ * @author SnappyS05 (thinh.nt233662@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-06
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include<stdio.h>
 int main(){
-   printf("Xin chào SRC!")
+   printf("Xin chào SRC!");
    return 0;
 }
-/* Trả lời câu hỏi ở đây:
-
-
-*/
