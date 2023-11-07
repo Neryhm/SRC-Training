ba4b6f771b5ee203987a353cf43e0d127a028a9b
Nguyen Trung Kien <kienxoi1508@gmail.com>
2023-11-07 21:25:36 +0700

    Nguyen Trung Kien nop bai

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 int main(){
-   printf("Xin chào SRC!")
+   printf("Xin chào SRC!");
    return 0;
 }
 /* Trả lời câu hỏi ở đây:
-
-
+so với trước đây thì mình cảm thấy bản thân còn thiếu rất nhiều
+kiến thức để có thể làm việc nhóm với mọi người!
+ở đây mình cũng đã được các bạn chia sẻ thêm rất nhiều tài liệu cũng như kinh nghiệm
+làm việc của các bạn! Do mới vào chưa lâu nên chưa có nhiều kỉ niệm cùng mọi người nên
+mình mong trong tương lai mình có thể cùng làm việc với mọi người và có thêm nhiều
+kỉ niệm hơn !
 */
