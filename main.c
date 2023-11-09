3fe08445f55cadc06613b22eb30f110375edf436
DoanCuong24 <150043850+DoanCuong24@users.noreply.github.com>
2023-11-09 18:18:17 +0700

    DoanCuong

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(){
-   printf("Xin chào SRC!")
+   printf("Xin chào SRC!");
    return 0;
 }
-/* Trả lời câu hỏi ở đây:
+/* Trước khi vào clb đã mê rồi còn giờ là mê vcll
+Bản thân em biết nhiều thứ mới hơn, lạ hơn, thú vị hơn, gặp được nhiều anh chị, bạn bè số 1 luôn ạ
 
 
 */
